c588e1f8093c73d3bda1ce408f710c0a51fd202e
luken <lukenewman@live.com.au>
2018-10-21 21:02:20 +1100

    remove redundant clingo, start on exec

--- src/scripts/gripper.js
+++ src/scripts/gripper.js
@@ -90,25 +90,27 @@ class Gripper_GUI {
         if (err) throw err;
         console.log("Done writing.");
       });
-      // execute solver and parser
-      // const { exec } = require("child_process");
-      // exec(
-      //   '"./clingo blocks_ASP_prog.lp instances.inp > out.inp"',
-      //   {
-      //     cwd: "./Clingo"
-      //   },
-      //   function(err, stdout) {
-      //     exec("./clingo/parser",
-      //     {
-      //       cwd: "./Clingo"
-      //     },
-      //     function(err, stdout) {
-      //       console.log(stdout);
-      //     });
-      //     console.log(stdout);
-      //     alert("Solver completed");
-      //   }
-      // );
+      //execute solver and parser
+      const { exec } = require("child_process").exec;
+      exec(
+        '"./clingo blocks_ASP_prog.lp instances.inp > out.inp"',
+        {
+          cwd: "./Clingo"
+        },
+        function(err, stdout) {
+          exec(
+            "./clingo/parser",
+            {
+              cwd: "./Clingo"
+            },
+            function(err, stdout) {
+              console.log(stdout);
+            }
+          );
+          console.log(stdout);
+          alert("Solver completed");
+        }
+      );
     }
 
     this.resetBlocks();
