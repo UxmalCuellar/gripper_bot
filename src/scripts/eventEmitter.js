3ed85a279fbdbe35f64fdf0f59a03608cfb9d324
luken <lukenewman@live.com.au>
2018-10-24 22:23:44 +1100

    attempt to clean dirty out.inp file

--- src/scripts/eventEmitter.js
+++ src/scripts/eventEmitter.js
@@ -1,5 +1,7 @@
 import { EventEmitter } from "events";
+import { app } from "electron";
 import Parser from "./parser";
+var fs = require("fs");
 /*
     This module provides a way to sequence the asynchronous execution of
     external programs
@@ -15,6 +17,11 @@ emitter.on("clingo-finished", arg => {
 
 emitter.on("parser-finished", arg => {
   console.log("from emitter - parser exit with code: " + arg);
+
+  // clear out.inp
+  fs.truncate(`${app.getAppPath()} + /../Clingo/out.inp`, 0, function() {
+    console.log("out.inp cleaned");
+  });
 });
 
 export const eventEmitter = emitter;
